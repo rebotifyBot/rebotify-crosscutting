940dbc20cb606e06e046b4c2ef177ef2a76ac1b8
KickTricks <kicktricks@outlook.com>
2024-03-08 12:40:45 +0100

    Adding platform and eventtype enums

---
+++ src/enums/Platform.ts
@@ -0,0 +1,6 @@
+export enum Platform {
+  Internal,
+  Kick,
+  Discord,
+  Twitch
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export * from './enums/ErrorLogMessageOrigin'
+export * from './enums/EventType'
+export * from './enums/Platform'
 export * from './interfaces/IErrorLogMessage'
